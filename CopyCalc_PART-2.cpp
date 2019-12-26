4a87d056870a5e610528cdc004333e8ef810e537
VietTV123 <47969764+VietTV123@users.noreply.github.com>
2019-12-26 02:14:33 +0200

    Add files via upload

---
+++ CopyCalc_PART-2.cpp
@@ -0,0 +1,15 @@
+#include "CopyCalc.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int positive_integer;
+	cout<<"Enter a positive integer: "<<endl;
+	cin>>positive_integer;
+	cout<<"The factorial of " << positive_integer << " is: " <<  factorial(positive_integer) <<endl;
+
+	return 0;
+}
+
